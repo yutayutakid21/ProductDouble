5dadc705d52ca7fb8b0244788da35c290c7b69e1
Yuta Fujii <yutayutakid21@gmail.com>
2020-02-04 18:23:15 +0900

    課題です！

---
+++ ProductDouble/KondoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  KondoViewController.swift
+//  ProductDouble
+//
+//  Created by Fujii Yuta on 2020/02/04.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class KondoViewController: UIViewController {
+
+    /*
+     
+     課題 tableViewを作成し、駅名5つを入れたセルを準備し、
+     セルタップでNakatsukaViewControllerへlabelの値を渡しながら画面遷移
+     (NakatsukaViewControllerが受ける変数名はpassText)
+     
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProductDouble/NakatsukaViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NakatsukaViewController.swift
+//  ProductDouble
+//
+//  Created by Fujii Yuta on 2020/02/04.
+//  Copyright © 2020 Fujii Yuta. All rights reserved.
+//
+
+import UIKit
+
+class NakatsukaViewController: UIViewController {
+
+    /*
+     
+     ・近藤さんがpassTextという値を渡してくるので、
+     ・passTextというString型の変数を準備し、labelへ表示
+     ・下部へボタンを設置しタップでdissmissによって戻る
+     ・toolbarを設定し、そちらにも渡ってきた値をタイトルとして表示
+     
+     */
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
