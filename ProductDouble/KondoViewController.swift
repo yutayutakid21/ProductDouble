f0c66e50467d1b94be6af8fd7b1b53b284fdeb11
Hiroki Kondo <kondo.matr02@gmail.com>
2020-02-04 18:51:30 +0900

    完了しました

--- ProductDouble/KondoViewController.swift
+++ ProductDouble/KondoViewController.swift
@@ -8,39 +8,60 @@
 
 import UIKit
 
-class KondoViewController: UIViewController {
+class KondoViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-    
-
     /*
-     
-     課題 tableViewを作成し、駅名5つを入れたセルを準備し、
-     セルタップでNakatsukaViewControllerへlabelの値を渡しながら画面遷移
-     (NakatsukaViewControllerが受ける変数名はpassText)
-     
-     */
+        
+        課題 tableViewを作成し、駅名5つを入れたセルを準備し、
+        セルタップでNakatsukaViewControllerへlabelの値を渡しながら画面遷移
+        (NakatsukaViewControllerが受ける変数名はpassText)
+        
+        */
+       
+       //課題、承知しました。
+       
     
-    //課題、承知しました。
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var indexNumber = 0
     
     var stationArray = ["新宿","渋谷","恵比寿","五反田","高輪ゲートウェイ"]
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stationArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = stationArray[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        indexNumber = indexPath.row
+        performSegue(withIdentifier: "next", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nakatsukaVC = segue.destination as! NakatsukaViewController
+               nakatsukaVC.passText = stationArray[indexNumber]
+    }
 
 }
